9de1fdc5d5689403c4524af46aa935ecf08afdf3
Pavithra <pavithra@identifyyou.in>
2022-12-23 16:42:26 +0530

    index page commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-$(document).ready(function () {
-  document.getElementById("name").innerText = "Hi";
+import { createServer } from "http";
+const server = createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello Wolrd");
 });
+const port = process.env.port || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
